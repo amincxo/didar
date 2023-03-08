58835588acf7014af8ed5b9559e7077cc39f1129
amincxo <amincxo@gmail.com>
2023-03-09 00:10:21 +0330

    First Commit

---
+++ server/app.js
@@ -0,0 +1,36 @@
+var url = require('url');
+var fs = require('fs');
+
+
+function renderHTML(path, response){
+    fs.readFile(path,null,function(error , data){
+        if (error) {
+            response.writeHead(404);
+            response.write('file not find');
+        }else{
+            response.write(data);
+        }
+        response.end();
+    });
+}
+
+module.exports = {
+    handleRequest: function(request,response) {
+        response.writeHead(200,{'Contant-Type': 'text/html'});
+
+        var path = url.parse(request.url).pathname;
+        switch(path){
+            case '/':
+                renderHTML('./index.html',response);
+                break;
+            case '/vr':
+                renderHTML('./vr.html', response);
+                break;
+            default:
+                response.writeHead(404);
+                response.write('route not defined');
+                response.end();
+                
+        }
+    }
+};
